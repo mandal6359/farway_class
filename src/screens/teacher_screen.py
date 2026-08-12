4bec20b5ce3fc52d94775f6a7dd091fb9da368e0
Piyush Mandal <mandalpiyush02052004@gmail.com>
2026-08-12 22:47:08 +0530

    Build_Homescreen + Design_teacher_authportal + databasesetup + database_function + setup_teacher_authenticatin

--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -2,21 +2,36 @@ import streamlit as st
 from src.ui.base_layout import style_background_dashboard, style_base_layout
 from src.components.header import header_dashboard
 from src.components.footer import footer_dashboard
+from src.database.db import check_teacher_exists, create_teacher, teacher_login
+
 
 def teacher_screen():
     style_background_dashboard()
     style_base_layout()
 
-    
-
-    if 'teacher_login_type' in st.session_state or st.session_state.teacher_login_type=="login":
+    if "teacher_data" in st.session_state:
+        teacher_dashboard()
+    elif 'teacher_login_type' not in st.session_state or st.session_state.teacher_login_type =="login":
         teacher_screen_login()
     elif st.session_state.teacher_login_type == "register":
         teacher_screen_register()
 
+def teacher_dashboard():
+    teacher_data = st.session_state.teacher_data
+    st.header(f"""Welcome, {teacher_data['name']}""")
 
+def login_teacher(username, password):
+    if not username or not password:
+        return False
+    teacher = teacher_login(username, password)
 
+    if teacher:
+        st.session_state.user_role ='teacher'
+        st.session_state.teacher_data = teacher
+        st.session_state.is_logged_in = True
+        return True
 
+    return False
 
 def teacher_screen_login():
     c1, c2 = st.columns(2, vertical_alignment='center', gap='xlarge')
@@ -56,6 +71,20 @@ def teacher_screen_login():
 
     footer_dashboard()
 
+def  register_teacher(teacher_name, teacher_username, teacher_pass, teacher_pass_confirm):
+    if not teacher_username or not teacher_name or not teacher_pass:
+        return False, "ALL FIELDS ARE REQUIRED !"
+    if check_teacher_exists(teacher_username):
+        return False, "USERNAME ALREADY TAKEN"
+    if teacher_pass != teacher_pass_confirm:
+        return False, "PASSWORD DOSEN'T MATCH !"
+
+    try:
+        create_teacher(teacher_username, teacher_pass, teacher_name)
+        return True, "SUCCESFULLY CREATE! LOGIN NOW"
+    except Exception as e:
+        return False, "UNEXPECTED ERROR !!"
+
 
 def teacher_screen_register():
     c1, c2 = st.columns(2, vertical_alignment='center', gap='xlarge')
@@ -89,7 +118,16 @@ def teacher_screen_register():
     btnc1, btnc2 = st.columns(2)
 
     with btnc1:
-        st.button("Register Now", icon=":material/passkey:", shortcut="control+enter", width="stretch")
+        if st.button("Register Now", icon=":material/passkey:", shortcut="control+enter", width="stretch"):
+            success, message = register_teacher(teacher_name, teacher_username, teacher_pass, teacher_pass_confirm)
+            if success:
+                st.success(message)
+                import time
+                time.sleep(2)
+                st.session_state.teacher_login_type = "login"
+                st.rerun()
+            else:
+                st.error(message)
 
     with btnc2:
         if st.button("Login Instead", type ='primary', icon=":material/passkey:", width="stretch"):
