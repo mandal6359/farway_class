98306eb76f9ffc4e790080e751e729faa9624697
Piyush Mandal <mandalpiyush02052004@gmail.com>
2026-08-20 21:20:36 +0530

    Attandace repair

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -66,7 +66,7 @@ def style_base_layout():
                 font-size: 3.5rem !important;
                 line-height:1.1 !important;
                 margin-bottom: 0rem !important;
-                # color: black !important;
+                color: black !important;
             }
                 
 
